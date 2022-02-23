d02c2150584f3b75ddc982083da449551e2db94d
sajadAD <sajadeydi8026@gmail.com>
2022-02-23 07:24:09 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,70 @@
+import { useTranslation } from "react-i18next";
+import i18next from "i18next";
+import Cookies from "js-cookie";
+import { useEffect } from "react";
+
+const language = [
+  {
+    code: "fr",
+    name: "french",
+    country_code: "fr",
+  },
+  {
+    code: "en",
+    name: "english",
+    country_code: "gb",
+  },
+  {
+    code: "ar",
+    name: "العربی",
+    country_code: "ar",
+    dir: "rtl"
+  },
+];
+
 function App() {
+  const currentLanguageCode = Cookies.get('i18next') || 'en'
+  const currentLanguage = language.find( (elements) => elements.code === currentLanguageCode)
+  const { t } = useTranslation();
+  useEffect(() => {
+    document.body.dir = currentLanguage.dir || 'ltr'
+  }, [currentLanguage])
   return (
-    <div className="App">
-      Hello World
+    <div className="container d-flex">
+      <div className="d-flex justify-content-end">
+        <div className="dropdown">
+          <button
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton1"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Dropdown button
+          </button>
+          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+            {language.map(({ code, name, country_code }) => (
+              <li key={country_code}>
+                <button
+                  className="dropdown-item"
+                  onClick={() => {
+                    i18next.changeLanguage(code);
+                  }}
+                >
+                  <span
+                    className={`flag-icon flag-icon-${country_code} mx-2`}
+                  ></span>
+                  {name}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="d-flex flex-column align-items-start">
+        <h1 className="font-weight-normal md-3">{t("Welcome_to_React")}</h1>
+        <p>{t("language")}</p>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
 
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
+
+import "./index.css";
+import App from "./App";
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    // the translations
+    // (tip move them in a JSON file and import them,
+    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+    supportedLngs: ["en", "ar", "fr"],
+    fallbackLng: "en",
+    detection: {
+      order: ["cookie","htmlTag",  "localStorage", "path", "subdomain"],
+      caches: ["cookie"],
+    },
+    backend: {
+      loadPath: "/assets/locales/{{lng}}/translation.json",
+    },
+    // react: { useSuspense: false }, commente shode bad az etmam tamam kar ha
+  });
+const landingMarkup = ( 
+  <div className="py-4 text-center">
+    <h2>Loading ...</h2>
+  </div>
+)
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Suspense fallback={landingMarkup}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Suspense>,
+  document.getElementById("root")
 );
